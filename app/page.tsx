ef7c945535b4c6ad60cb549b3de3bc52ec8bdd8e
siriwatknp <siriwatkunaporn@gmail.com>
2025-06-02 14:42:16 +0700

    remove empty demo

--- app/page.tsx
+++ app/page.tsx
@@ -62,15 +62,6 @@ export default function Home() {
             <PokemonPage />
           </div>
         </div>
-
-        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A login form with a CSS file.
-            </h2>
-            <OpenInV0Button name="example-with-css" className="w-fit" />
-          </div>
-        </div>
       </main>
     </div>
   );
