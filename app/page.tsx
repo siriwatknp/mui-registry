589c29f557db48904cc1d9f6b5d232359cefe1fc
siriwatknp <siriwatkunaporn@gmail.com>
2025-06-02 13:38:02 +0700

    chore: remove simple hello block and its references

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { OpenInV0Button } from "@/components/open-in-v0-button";
-import { HelloWorld } from "@/registry/new-york/blocks/hello-world/hello-world";
 import { ExampleForm } from "@/registry/new-york/blocks/example-form/example-form";
 import PokemonPage from "@/registry/new-york/blocks/complex-component/page";
 import { ExampleCard } from "@/registry/new-york/blocks/example-with-css/example-card";
@@ -38,18 +37,6 @@ export default function Home() {
           </div>
           <div className="flex items-center justify-center min-h-[400px] relative">
             <LoginForm />
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A simple hello world component
-            </h2>
-            <OpenInV0Button name="hello-world" className="w-fit" />
-          </div>
-          <div className="flex items-center justify-center min-h-[400px] relative">
-            <HelloWorld />
           </div>
         </div>
 
