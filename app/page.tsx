fc0e3453571c02c18f804f1bacc87aed7033c80d
siriwatknp <siriwatkunaporn@gmail.com>
2025-06-02 17:37:46 +0700

    fix name

--- app/page.tsx
+++ app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
             <h2 className="text-sm text-muted-foreground sm:pl-3">
               A contact form with Zod validation.
             </h2>
-            <OpenInV0Button name="example-form" className="w-fit" />
+            <OpenInV0Button name="feedback-form" className="w-fit" />
           </div>
           <div className="flex items-center justify-center min-h-[500px] relative">
             <FeedbackForm />
