2a3d7414b0242ddf0ca3e027d9fe9eb472d10f0e
siriwatknp <siriwatkunaporn@gmail.com>
2025-06-05 11:03:12 +0700

    update title

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-svh px-4 py-8 gap-8">
       <header className="flex flex-col gap-1">
-        <h1 className="text-3xl font-bold tracking-tight">Custom Registry</h1>
+        <h1 className="text-3xl font-bold tracking-tight">MUI Registry</h1>
         <p className="text-muted-foreground">
-          A custom registry for distributing code using shadcn.
+          A registry for distributing MUI code.
         </p>
       </header>
       <main className="grid grid-cols-[repeat(auto-fill,minmax(min(100%,600px),1fr))] gap-4">
