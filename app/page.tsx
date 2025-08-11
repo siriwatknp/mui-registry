5ecd5f8f18be371fab43414921d79c16605d045e
Siriwat K <siriwatkunaporn@gmail.com>
2025-08-11 14:19:13 +0700

    feat: add product-card-01 component (#3)
    closes #2

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ import {
   GridWidget,
   GridWidgetItem,
 } from "@/registry/new-york/components/grid-widget/grid-widget";
+import { ProductCard01 } from "@/registry/new-york/components/product-card-01/product-card-01";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -119,6 +120,21 @@ export default function Home() {
               </Card>
             </GridWidgetItem>
           </GridWidget>
+        </PreviewFrame>
+        {/* Product Card */}
+        <PreviewFrame
+          name="product-card-01"
+          title="Product Card"
+          registryType="component"
+        >
+          <div className="flex items-center justify-center min-h-[400px] relative">
+            <ProductCard01
+              title="Snickers Off-White 2024"
+              brand="NIKE"
+              price="$38.00"
+              image="https://placehold.co/400x300/ff4500/ffffff?text=Sneaker"
+            />
+          </div>
         </PreviewFrame>
         {/* Booking Summary */}
         <PreviewFrame
