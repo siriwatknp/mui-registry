ea94bdd93279d24cf49e58233bf694c78ca39917
siriwatknp <siriwatkunaporn@gmail.com>
2025-08-21 18:24:54 +0700

    add payroll-summary-card

---
+++ app/payroll-summary-card/page.tsx
@@ -0,0 +1,10 @@
+import { PreviewComponent } from "@/components/preview-page";
+import PayrollSummaryCard from "@/registry/new-york/blocks/payroll-summary-card/payroll-summary-card";
+
+export default function Page() {
+  return (
+    <PreviewComponent>
+      <PayrollSummaryCard />
+    </PreviewComponent>
+  );
+}
