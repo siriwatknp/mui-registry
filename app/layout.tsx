adfa0f9f5555b7929faf843b95067044dd3159e2
siriwatknp <siriwatkunaporn@gmail.com>
2025-08-21 19:43:44 +0700

    add command-search-ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,12 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <InitColorSchemeScript attribute="class" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <InitColorSchemeScript attribute="class" />
         <AppRouterCacheProvider
           options={{
             enableCssLayer: true,
