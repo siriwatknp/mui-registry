f46a93cef96f11a45fe1b6c0618853351980cd03
siriwatknp <siriwatkunaporn@gmail.com>
2025-08-21 21:15:56 +0700

    update theme to make select height consistent with input

--- registry/new-york/themes/minimal/components/select.ts
+++ registry/new-york/themes/minimal/components/select.ts
@@ -8,27 +8,21 @@ export const selectTheme: ThemeOptions["components"] = {
     styleOverrides: {
       select: {
         "&.MuiOutlinedInput-input": {
-          padding: "8px 14px",
+          padding: "10px 14px",
+          minHeight: "1.42857em", // 20px
+          "&.MuiInputBase-inputSizeSmall": {
+            padding: "7px 14px",
+          },
         },
         "&.MuiFilledInput-input": {
           padding: "25px 12px 8px",
+          "&.MuiInputBase-inputSizeSmall": {
+            padding: "21px 10px 4px",
+          },
         },
         "&.MuiInputBase-input": {
           paddingRight: 32,
         },
-        variants: [
-          {
-            props: { size: "small" },
-            style: {
-              "&.MuiOutlinedInput-input": {
-                padding: "4px 14px",
-              },
-              "&.MuiFilledInput-input": {
-                padding: "21px 10px 4px",
-              },
-            },
-          },
-        ],
       },
       icon: ({ theme }) => ({
         color: (theme.vars || theme).palette.text.secondary,
